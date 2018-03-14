de73eb6910d9dd10d19310683344f9b0adae75e6
JonDuncanson-abertay <1600538@uad.ac.uk>
2018-03-14 12:29:12 +0000

    Threading
    Implemented a basic barrier in the Application class

--- Core/System/Application.cpp
+++ Core/System/Application.cpp
@@ -4,9 +4,14 @@
 
 namespace minish
 {
+    Application::Application(unsigned int thread_count)
+    {
+        m_threads.resize(thread_count - 1);
+    }
+
     void Application::syncThreads()
     {
-        if (m_threadsync < m_threads.size())
+        if (m_threadsync < m_threads.size() + 1) //takes into account main execution thread
         {
             std::this_thread::yield();
             m_sync_mutex.lock();
