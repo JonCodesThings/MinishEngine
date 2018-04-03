b3d2c278931e5a02f599361dc8ec4c8b4ce503d3
JonDuncanson-abertay <1600538@uad.ac.uk>
2018-04-03 14:36:01 +0100

    Subsystem overhaul
    Added dependencies for subsystems

--- Core/System/Subsystem.cpp
+++ Core/System/Subsystem.cpp
@@ -1,14 +1,38 @@
 #include "Subsystem.h"
 
+#include <thread>
+
 namespace minish
 {
-    bool Subsystem::getUpdateFlag()
+    void Subsystem::addDependency(Subsystem& dependency)
     {
-        return m_updated;
+        m_dependencies.insert(&dependency);
     }
 
-    void Subsystem::toggleUpdateFlag()
+    SUBSYSTEM_STATE Subsystem::getSubsystemState()
     {
-        m_updated = !m_updated;
+        return m_state;
+    }
+
+    void Subsystem::removeDependency(Subsystem& dependency)
+    {
+        m_dependencies.erase(&dependency);
+    }
+
+    void Subsystem::resetSubsystemState()
+    {
+        m_state = NOT_UPDATED;
+    }
+
+    void Subsystem::update(const float dt)
+    {
+        for (auto dependency : m_dependencies)
+        {
+            do 
+            {
+                std::this_thread::sleep_for(std::chrono::microseconds(10));
+            } while (dependency->getSubsystemState() != NOT_UPDATED);
+        }
+        updateSubsystem(dt);
     }
 }
--- Core/System/Subsystem.h
+++ Core/System/Subsystem.h
@@ -1,8 +1,18 @@
 #ifndef MINISH_SUBSYSTEM_H
 #define MINISH_SUBSYSTEM_H
 
+#include <unordered_set>
+
 namespace minish
 {
+
+    enum class SUBSYSTEM_STATE
+    {
+        NOT_UPDATED,
+        UPDATING,
+        UPDATED
+    };
+
     /*!
     * \brief Abstract class used for gameplay subsystems.
     * \author Jonathan Duncanson
@@ -15,25 +25,40 @@ namespace minish
         	*/
             virtual ~Subsystem();
 
-            /*!
-        	\brief Class member that returns the status of the update flag.
-        	*/
-            bool getUpdateFlag();
+            void addDependency(Subsystem& dependency);
 
             /*!
-        	\brief Class member that toggles the update flag. Called as each subsystem is updated and once all subsystems are updated.
+        	\brief Class member that returns the status of the subsystem.
         	*/
-            void toggleUpdateFlag();
+            SUBSYSTEM_STATE getSubsystemState();
+
+            void removeDependency(Subsystem& dependency);
+
+            /*!
+        	\brief Class member that resets the subsystem state back to NOT_UPDATED.
+        	*/
+            void resetSubsystemState();
+
+            /*!
+        	\brief Function called to update the subsystem, provided all the dependencies are updated. If not, the thread is put to sleep.
+        	*/
+            void update(const float dt);
+        protected:
 
             /*!
         	\brief Pure virtual class member for updating the subsystem.
         	*/
-            virtual void update(const float dt) = 0;
-        protected:
+            virtual void updateSubsystem(const float dt) = 0;
+
             /*!
-        	\brief Update flag that stores whether or not the subsystem has bee updated.
+        	\brief Stores the state of the subsystem.
         	*/
-            bool m_updated = false;
+            SUBSYSTEM_STATE m_state = NOT_UPDATED;
+
+            /*!
+        	\brief A list of the subsystem's dependencies.
+        	*/
+            std::unordered_set<Subsystem*> m_dependencies;
     };
 }
 
