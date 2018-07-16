2f2a6d57a3ebba4e532560f5d0a00095d10d841f
JonDuncanson-abertay <1600538@uad.ac.uk>
2018-07-16 16:13:10 +0100

    AudioComponent class added

---
+++ Core/Audio/AudioComponent.cpp
@@ -0,0 +1,25 @@
+#include "AudioComponent.h"
+
+namespace minish
+{
+    AudioComponent::AudioComponent() : m_audio_manager(nullptr) {}
+
+    void AudioComponent::playMusic(const std::string& id)
+    {
+        if (m_audio_manager)
+            m_audio_manager->playMusic(id);
+    }
+
+    void AudioComponent::playSound(const std::string& id)
+    {
+        if (m_audio_manager)
+            m_audio_manager->playSound(id);
+    }
+
+    void AudioComponent::setAudioManager(AudioManager& audio_manager)
+    {
+        m_audio_manager = &audio_manager;
+    }
+
+    void AudioComponent::update(const float dt) {}
+}
---
+++ Core/Audio/AudioComponent.h
@@ -0,0 +1,46 @@
+#ifndef MINISH_AUDIOCOMPONENT_H
+#define MINISH_AUDIOCOMPONENT_H
+
+#include "AudioManager.h"
+#include <Core/Entity/Component.h>
+
+namespace minish
+{
+    class AudioComponent : public Component
+    {
+    public:
+        /*!
+        \brief Default constructor.
+        */
+        AudioComponent();
+
+        /*!
+        \brief Virtual destructor.
+        */
+        virtual ~AudioComponent() {};
+
+        /*!
+        \brief Class member that plays music on the current audio manager if available.
+        */
+        void playMusic(const std::string& id);
+
+        /*!
+        \brief Class member that plays a sound on the current audio manager if available.
+        */
+        void playSound(const std::string& id);
+
+        /*!
+        \brief Class member that sets the current audio manager.
+        */
+        void setAudioManager(AudioManager& audio_manager);
+
+        /*!
+        \brief Updates the audio component.
+        */
+        virtual void update(const float dt);
+    private:
+        AudioManager* m_audio_manager;
+    };
+}
+
+#endif
